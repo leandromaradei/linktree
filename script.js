8840533f889491dcd8664c741b9768039c6a05cc
Leandro Padula Maradei <leandro.maradei@startyourapp.com.br>
2023-08-21 17:14:26 -0300

    add js buttom function
    Aqui é um template commit-msg

---
+++ script.js
@@ -0,0 +1,16 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag image
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagen
+  if (html.classList.contains("light")) {
+    // se estiver light mode, adicionar a imagem 'light'
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se não estiver light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
